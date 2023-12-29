ca814ebe552b2c38f1bbf3a365b8e5650def88ed
GWjun <kb865900@gmail.com>
2023-12-29 11:01:41 +0900

    3-Add SearchForm

--- 3-component/src/App.js
+++ 3-component/src/App.js
@@ -1,8 +1,43 @@
 import React from "react";
 import Header from "./components/Header.js";
+import SearchForm from "./components/SearchForm.js";
 
 export default class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            searchKeyword: "",
+        }
+    }
+
+    handleChangeInput(searchKeyword) {
+        if (searchKeyword <= 0) {
+            this.handleReset();
+        }
+        this.setState({ searchKeyword });
+    }
+
+    search(searchKeyword) {
+        console.log("search");
+    }
+
+    handleReset() {
+        console.log("reset");
+    }
+
     render() {
-        return <Header title="검색"/>;
+        return (
+            <>
+                <Header title="검색" />
+                <div className="container">
+                    <SearchForm 
+                        value={this.state.searchKeyword}
+                        onChange={(value) => { this.handleChangeInput(value) }}
+                        onSubmit={(searchKeyword) => this.search(searchKeyword)}
+                        onReset={() => this.handleReset()}
+                    />
+                </div>
+            </>
+        );
     }
 }
